9f9638b2f2d57c5f3097502dd82b3c58fc0401e0
Tak <env.tak@gmail.com>
2022-09-13 15:36:48 +0900

    feat: rebuilding site Tue Sep 13 15:36:48 KST 2022

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -28,7 +28,7 @@ const SHELL_CACHE = "shell-8.5.2--v1--sw/";
 const ASSETS_CACHE = "assets--v1--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2020-12-02T13:44:42+09:00--sw/";
+const CONTENT_CACHE = "content--2022-09-13T15:36:36+09:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const CACHE_SEARCH_PARAM = "sw-cache";
--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-importScripts("/assets/js/sw.js?t=2020-12-02T13:44:42+09:00");
+importScripts("/assets/js/sw.js?t=2022-09-13T15:36:36+09:00");
